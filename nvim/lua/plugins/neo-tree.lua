c728c5c2e1a8eaedc96dfa1ab9a689e3d3bcfd03
Noah Gorstein <ngorstein@gmail.com>
2023-05-02 21:30:06 -0400

    updates

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,26 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  -- cmd = "Neotree",
-  -- keys = {
-  --   { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "Neotree" },
-  -- },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = {
-    container = {
-      enable_character_fade = true
-    },
-    filesystem = {
-      hide_dotfiles = false,
-      follow_current_file = true,
-      hijack_netrw_behavior = "open_current",
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	-- cmd = "Neotree",
+	-- keys = {
+	--   { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "Neotree" },
+	-- },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = {
+		container = {
+			enable_character_fade = true,
+		},
+		filesystem = {
+			follow_current_file = true,
+			hijack_netrw_behavior = "open_current",
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+			},
+		},
+	},
 }
