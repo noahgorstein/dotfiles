aa1cf59e9c4798f6cc75699692bc62c65edcc536
Noah Gorstein <noahgorstein@stardog.com>
2024-01-11 11:04:17 -0500

    work updates

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,36 +1,35 @@
 return {
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
 
-	"tpope/vim-sleuth",
-	"tpope/vim-surround",
+    'tpope/vim-sleuth',
+    'tpope/vim-surround',
+    'towolf/vim-helm',
 
-	{ "folke/which-key.nvim", opts = {} },
+    { 'folke/which-key.nvim',  opts = {} },
 
-	{ "numToStr/Comment.nvim", opts = {} },
+    { 'numToStr/Comment.nvim', opts = {} },
 
-	{
-		"simrat39/symbols-outline.nvim",
-		config = function()
-			require("symbols-outline").setup()
-		end,
-	},
+    {
+        'simrat39/symbols-outline.nvim',
+        config = function()
+            require('symbols-outline').setup()
+        end
+    },
 
-	"vim-test/vim-test",
-	{
-		"iamcco/markdown-preview.nvim",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-		cmd = {
-			"MarkdownPreview",
-			"MarkdownPreviewStop",
-			"MarkdownPreviewToggle",
-		},
-		event = "BufRead",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    'vim-test/vim-test',
+    {
+        "iamcco/markdown-preview.nvim",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+        cmd = {
+            "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle"
+        },
+        event = "BufRead",
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
