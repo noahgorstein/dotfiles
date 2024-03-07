ea95a3b1cfb89a354bb47d85202bdfa36ffd4893
Noah Gorstein <noah.gorstein@stardog.com>
2024-03-07 10:14:52 -0500

    nvim: tweak lualine file status

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -7,6 +7,15 @@ return {
 				component_separators = "|",
 				section_separators = "",
 			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+					},
+				}
+			}
 		})
 	end,
 }
